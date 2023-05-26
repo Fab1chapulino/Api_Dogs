04e8fa15fa7c6f312c13d9e2ef5546e54419c371
Fabian Romero <chapulinozeta@gmail.com>
2023-05-26 18:36:56 -0400

    Added a controller

--- src/module/temperaments/temperaments.module.ts
+++ src/module/temperaments/temperaments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { temperamentsProviders } from './temperaments.providers';
 import { TemperamentsService } from './temperaments.service';
+import { TemperamentsController } from './temperaments.controller';
 
 @Module({
   providers: [...temperamentsProviders, TemperamentsService],
+  controllers: [TemperamentsController],
 })
 export class TemperamentsModule {}
