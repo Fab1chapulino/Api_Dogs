c39ee35418077bad8e3f7cc747119aeba4a0882f
Fabian Romero <chapulinozeta@gmail.com>
2023-05-26 14:03:21 -0400

    Added db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './core/database/database.module';
+import { TemperamentsModule } from './module/temperaments/temperaments.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DatabaseModule,
+    TemperamentsModule,
   ],
   controllers: [],
   providers: [],
