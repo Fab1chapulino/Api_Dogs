07979d41dcd953902eb52cae57e7767a7dbcebd8
Fabian Romero <chapulinozeta@gmail.com>
2023-05-27 12:19:10 -0400

    Added breedModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './core/database/database.module';
 import { TemperamentsModule } from './module/temperaments/temperaments.module';
+import { BreedsModule } from './module/breeds/breeds.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TemperamentsModule } from './module/temperaments/temperaments.module';
     }),
     DatabaseModule,
     TemperamentsModule,
+    BreedsModule,
   ],
   controllers: [],
   providers: [],
