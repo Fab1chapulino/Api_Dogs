a976ae9565093e8dd42fd25c93967e6f297d794a
Fabian Romero <chapulinozeta@gmail.com>
2023-05-27 12:32:33 -0400

    Created a breedsTemperaments model

---
+++ src/core/throughModels/breeds_temps.entity.ts
@@ -0,0 +1,19 @@
+import { Model, Table, Column, DataType, ForeignKey } from 'sequelize-typescript';
+import { Breed } from 'src/module/breeds/breeds.entity';
+import { Temperament } from 'src/module/temperaments/temperaments.entity';
+
+@Table({
+  timestamps:false,
+})
+export class BreedsTemperaments extends Model<BreedsTemperaments> {
+  @ForeignKey(() => Breed)
+  @Column({
+    type:DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+    breedId;
+
+  @ForeignKey(() => Temperament)
+  @Column
+    temperamentId:number;
+}
