db61fb3a001d0fa87a3aae08c59c0bc98c4ac52b
Fabian Romero <chapulinozeta@gmail.com>
2023-05-27 12:16:58 -0400

    Created breed module and entity

---
+++ src/module/breeds/breeds.entity.ts
@@ -0,0 +1,43 @@
+import { Column, Model, Table, DataType, BelongsToMany } from 'sequelize-typescript';
+import { Temperament } from '../temperaments/temperaments.entity';
+import { BreedsTemperaments } from '../../core/throughModels/breeds_temps.entity';
+
+@Table({
+  timestamps:false,
+})
+export class Breed extends Model<Breed> {
+  @Column({
+    type:DataType.UUID,
+    defaultValue:DataType.UUIDV4, // Or DataTypes.UUIDV1
+    primaryKey:true,
+  })
+    id;
+
+  @Column({
+    type:DataType.STRING,
+    unique:true,
+    allowNull:false,
+  })
+    name;
+
+  @Column({
+    type:DataType.STRING,
+    allowNull:false,
+  })
+    height;
+
+  @Column({
+    type:DataType.STRING,
+    allowNull:false,
+  })
+    weight;
+
+  @Column({
+    type:DataType.STRING,
+    allowNull:false,
+  })
+    life_span;
+
+  @BelongsToMany(() => Temperament, () => BreedsTemperaments)
+    temperaments: Temperament[];
+}
