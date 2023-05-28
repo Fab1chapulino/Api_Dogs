04b9d490d66e6eb626bf51d2461fe5ac6afffd84
Fabian Romero <chapulinozeta@gmail.com>
2023-05-28 18:17:27 -0400

    Added imports, exports, providers and controllers

--- src/module/breeds/breeds.module.ts
+++ src/module/breeds/breeds.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { breedsProviders } from './breeds.providers';
+import { BreedsService } from './breeds.service';
+import { BreedsController } from './breeds.controller';
+import { TemperamentsModule } from '../temperaments/temperaments.module';
 
-@Module({})
+@Module({
+  imports:[TemperamentsModule],
+  providers:[...breedsProviders, BreedsService],
+  exports:[...breedsProviders],
+  controllers: [BreedsController],
+})
 export class BreedsModule {}
